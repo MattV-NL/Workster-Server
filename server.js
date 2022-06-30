a4958f2c210f93e2e6a0e90b06ba405843a5e2b7
mgorgeov97 <matthewggvey87@hotmail.com>
2022-06-30 21:14:11 -0230

    examples done during meeting with get coding

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  throw new Error('my err');
   res.download('server.js');
   res.render('index', { text: 'world' });
 });
+
+console.log(process.env.Foo);
 
 app.listen(3000);
