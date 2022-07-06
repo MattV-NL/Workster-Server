14191f4e0d7ece5a71d8ad1807e386376a4d1d9b
mgorgeov97 <matthewggvey87@hotmail.com>
2022-07-06 20:41:48 -0230

    minor changes

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.get('/', async (req, res) => {
-  throw new Error('my err');
-  res.download('server.js');
-  res.render('index', { text: 'world' });
-});
-
 console.log(process.env.Foo);
 
 app.listen(3000);
