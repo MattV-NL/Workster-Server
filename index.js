538675609b319c6b7e432bca0a7d42c1d9bc05ac
mgorgeov97 <matthewggvey87@hotmail.com>
2022-07-19 22:16:55 -0230

    attempted to use node-fetch but may be better to use axios or https instead, will look into it as function is not working as of now

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const https = require('https');
 const cors = require('cors');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 // const corsOptions = {
@@ -15,48 +16,68 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-let lang = 'en';
-let units = 'metric';
+const lang = 'en';
+const units = 'metric';
 const key = process.env.API_KEY;
-const lat = 47.63;
-const lon = -52.64;
+// const lat = 47.63;
+// const lon = -52.64;
 
-const options = {
-  hostname: `api.openweathermap.org`,
-  port: 443,
-  path: `/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang${lang}`,
-  method: 'GET',
-};
+// let options = {
+//   hostname: `api.openweathermap.org`,
+//   port: 443,
+//   path: `/data/2.5/onecall?lat=${userLoc.lat}&lon=${userLoc.lon}&appid=${key}&units=${units}&lang${lang}`,
+//   method: 'GET',
+// };
 
 // working on getting position from client
-app.post('/api/position', (req, res) => {
+app.get('/api/weather2', (req, res) => {
   console.log('i got a request');
-  console.log(req.body);
+  userLoc = req.body;
+  console.log(userLoc);
   res.json({
     status: 'success',
   });
 });
+
+app.get('/api/weather2/:latlon', async (req, res) => {
+  console.log(req.params);
+  const latlon = req.params.latlon.split(',');
+  console.log(latlon);
+  const lat = latlon[0];
+  const lon = latlon[1];
+  console.log(lat, lon);
+  const apiUrl = `api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang${lang}`;
+  const weatherResponse = await fetch(apiUrl);
+  const weatherData = await weatherResponse.json();
+
+  const data = {
+    weather: weatherData,
+  };
+
+  res.json(data);
+});
+
 // end of new
 
-app.get('/api/weather', (req, res) => {
-  let data = '';
-  const request = https.request(options, (response) => {
-    console.log(`statusCode: ${res.statusCode}`);
+// app.get('/api/weather', (req, res) => {
+//   let data = '';
+//   const request = https.request(options, (response) => {
+//     console.log(`statusCode: ${res.statusCode}`);
 
-    response.on('data', (d) => {
-      process.stdout.write(d);
-      data = data + d;
-    });
-  });
+//     response.on('data', (d) => {
+//       process.stdout.write(d);
+//       data = data + d;
+//     });
+//   });
 
-  request.on('end', () => {
-    res.status(200).json(JSON.parse(data));
-  });
+//   request.on('end', () => {
+//     res.status(200).json(JSON.parse(data));
+//   });
 
-  request.on('error', (err) => {
-    console.error(err);
-    res.status(500).send('internal server error');
-  });
+//   request.on('error', (err) => {
+//     console.error(err);
+//     res.status(500).send('internal server error');
+//   });
 
-  request.end();
-});
+//   request.end();
+// });
