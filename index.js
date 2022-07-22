0d82b789d12d7f4831f0043e4efc4f9eaee2da97
mgorgeov97 <matthewggvey87@hotmail.com>
2022-07-21 22:25:19 -0230

    work from todays meeting, working with API calls and server-client communication

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
-const https = require('https');
+const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -14,31 +18,21 @@ app.listen(PORT, () => {
 const lang = 'en';
 const units = 'metric';
 const key = process.env.API_KEY;
-//https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang${lang}
-let weatherData;
 
-app.get('/api/weather/:latlon', async (req, res) => {
+app.get('/api/weather/:latlon', async (req, resp) => {
   const latlon = req.params.latlon.split(',');
   const lat = latlon[0];
   const lon = latlon[1];
-  console.log(lat, lon);
-
-  const optionsGet = {
-    hostname: 'api.openweathermap.org',
-    port: 443,
-    path: `/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang${lang}`,
-    method: 'GET',
-  };
-
-  const request = await https.request(optionsGet, (response) => {
-    console.log('success');
-    response.on('data', (d) => {
-      weatherData += d;
-      console.log(weatherData);
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang${lang}`
+    );
+    resp.send(response.data);
+  } catch (err) {
+    resp.send({
+      message: 'server error',
+      status: 500,
     });
-  });
-
-  request.on('error', (error) => console.error(error));
-
-  request.end();
+    console.error(err);
+  }
 });
