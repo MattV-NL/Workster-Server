289ef47545bc80648c614635b3286148f94e8e61
Matt Vey <matthewvey@outlook.com>
2022-08-09 21:37:20 -0230

    began work on authentication feature, basic setup to get the ball rolling currently

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const corsOptions = {
   origin: 'http://localhost:3000',
 };
 
-const client = new Client({
+const db = new Client({
   host: 'localhost',
   user: 'postgres',
   port: 5432,
@@ -18,16 +18,7 @@ const client = new Client({
   database: 'wwadb',
 });
 
-client.connect();
-
-client.query(`Select * from users`, (err, res) => {
-  if (!err) {
-    console.log(res);
-  } else {
-    console.error(err);
-  }
-  client.end;
-});
+db.connect();
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -55,4 +46,39 @@ app.get('/api/weather/:latlon', async (req, resp) => {
     });
     console.error(err);
   }
+});
+
+app.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    `INSERT INTO users (username, password) VALUES ($1, $2)`,
+    [username, password],
+    (err, res) => {
+      console.log(err);
+    }
+  );
+  db.end;
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    `SELECT * FROM users WHERE username = $1 AND password = $2`,
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.rows.length > 0) {
+        res.send(result.rows[0]);
+      } else {
+        res.send({ message: 'Wrong username and password combination' });
+      }
+    }
+  );
+  db.end;
 });
