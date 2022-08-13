d846d25db26e5b3411d1d4e2f9c2a48e81035e53
Matt Vey <matthewvey@outlook.com>
2022-08-13 17:05:53 -0230

    set up a hashing system for passwords for the db

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const PORT = process.env.PORT || 8000;
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 dayjs.extend(utc);
+const bcrypt = require('bcrypt');
 
 const corsOptions = {
   origin: 'http://localhost:3000',
 };
 
-const db = new Client({
+const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
   port: 5432,
   password: process.env.POSTGRES_PW,
   database: 'wwadb',
+  max: 10,
+  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 0,
 });
 
 app.use(cors(corsOptions));
@@ -49,40 +53,65 @@ app.get('/api/weather/:latlon', async (req, resp) => {
   }
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
   const timestamp = dayjs.utc().format('YYYY-MM-DD HH:mm:ss').toString();
-  db.connect();
-  db.query(
-    `INSERT INTO users (username, email, password, created_on) VALUES ($1, $2, $3, $4)`,
-    [username, email, password, timestamp],
-    (err, res) => {
-      console.log(err);
-    }
-  );
-  res.send({ message: 'registration successful' });
-  db.end();
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const results = await pool.query(
+      `INSERT INTO users (username, email, password, created_on) VALUES ($1, $2, $3, $4)`,
+      [username, email, hashedPassword, timestamp],
+      (err, result) => {
+        console.log(err);
+      }
+    );
+    res.send({ message: 'registration successful' });
+  } catch {
+    res.status(500).send();
+  }
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const timestamp = dayjs.utc().format('YYYY-MM-DD HH:mm:ss').toString();
-  db.connect();
-  db.query(
+
+  const getPassword = await pool.query(
+    'SELECT username, password FROM users',
+    (err, result) => {
+      const user = result.rows.find((user) => user.username === username);
+      if (err) {
+        res.send(err);
+      }
+      try {
+        const check = bcrypt.compare(user.password, password);
+        if (check) {
+          res.send({ message: 'login successful' });
+        } else {
+          res.send('wrong username and password combination');
+        }
+      } catch {
+        res.status(500).send();
+      }
+    }
+  );
+
+  const results = await pool.query(
     'UPDATE users SET last_login = $1 WHERE username = $2 AND password = $3;',
     [timestamp, username, password],
     (err, result) => {
       if (err) {
         res.send(err);
-      } else if (result.rows === 1) {
-        res.send(result);
+      } else if (result.rowCount === 1) {
+        res.send({ message: 'login successful' });
       } else {
-        res.send({ message: 'Wrong username and password combination' });
+        res.send({
+          message: 'Wrong username and password combination',
+        });
       }
-      db.end();
     }
   );
 });
