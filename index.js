1df3ce81b230602573a733267228e1d8614224f7
Matt Vey <matthewvey@outlook.com>
2022-09-03 15:05:39 -0230

    created query to get saved work information from db to display for client

--- index.js
+++ index.js
@@ -246,3 +246,13 @@ app.post('/save_work_information', async (req, res) => {
     res.status(500).send({ message: 'opps something went wrong', error: err });
   }
 });
+
+app.get('/get_work_information/:location_id', async (req, res) => {
+  const location_id = req.params.location_id;
+  const work_information = await pool.query(
+    'SELECT * FROM work_information WHERE location_id = $1',
+    [location_id]
+  );
+
+  res.send(work_information.rows);
+});
