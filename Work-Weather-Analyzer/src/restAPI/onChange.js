82e2698c2ae87406cadd9db36b7809d04f417681
Matt Vey <matthewvey@outlook.com>
2022-10-28 21:29:51 -0230

    removed front end from repository

--- Work-Weather-Analyzer/src/restAPI/onChange.js
+++
@@ -1,8 +0,0 @@
-export const onChange = ({ setterFunction, isBoolean = false }) => {
-  if (isBoolean) {
-    return (e) => setterFunction(e);
-  } else {
-    return ({ target: { value } }) =>
-      setterFunction(isBoolean ? value !== true.toString() : value);
-  }
-};
