644b5c2c37657900ed93fddf45bc817f23d9e045
Matt Vey <matthewvey@outlook.com>
2022-11-05 13:50:56 -0230

    .

--- index.js
+++ index.js
@@ -48,14 +48,14 @@ app.use('/', express.static(static_dir));
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// commented out for dev environment
+// since cert is on VM
 const certPath = '/etc/letsencrypt/live/workster.app/fullchain.pem';
 const keyPath = '/etc/letsencrypt/live/workster.app/privkey.pem';
-
 const httpsOptions = {
   cert: fs.readFileSync(certPath),
   key: fs.readFileSync(keyPath),
 };
-
 https.createServer(httpsOptions, app).listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
