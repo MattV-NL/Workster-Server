af7752a3639ac4e02edeacc9e86e3069d2233453
Matt Vey <matthewvey@outlook.com>
2023-01-11 22:13:37 -0330

    added jest, begining to write unit tests

---
+++ __tests__/utilFunc/checkForSavedData.test.js
@@ -0,0 +1,29 @@
+jest.mock('pg');
+const checkForSavedData = require('../../utilFunc/checkForSavedData');
+const { Pool } = require('pg');
+
+test('should return true if saved data exists', async () => {
+  const targetTable = 'test';
+  const givenUserId = 0;
+  //   const pool = {
+  //     query: () => {
+  //       return {
+  //         rows: [
+  //           {
+  //             user_id: givenUserId,
+  //           },
+  //         ],
+  //       };
+  //     },
+  //   };
+  const pool = new Pool({});
+  pool.query.mockResolvedValueOnce({ rows: [{ user_id: givenUserId }] });
+  const result = await checkForSavedData(givenUserId, pool, targetTable);
+  // check return value
+  expect(result).toBe(true);
+  // check mocks
+  expect(pool.query).toHaveBeenCalledTimes(1);
+  expect(pool.query).toHaveBeenLastCalledWith(
+    `SELECT user_id FROM ${targetTable}`
+  );
+});
