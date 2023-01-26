38a8da9224c8642ca9cc7b0fb0618f6ec7e433e5
Matthew Vey <87041586+MattV-NL@users.noreply.github.com>
2023-01-26 11:06:23 -0330

    removing supertest to fix open handle error

--- __tests__/endpointTests/login.test.js
+++ __tests__/endpointTests/login.test.js
@@ -1,8 +1,21 @@
-const request = require('supertest');
 const app = require('../../src/index');
+const axios = require('axios');
+jest.mock(axios);
 
-test('testing login endpoint', async () => {
-  const response = await request(app).post('/api/login').send({
+beforeAll(() => {
+  app.listen(4000, () => {
+    console.log('server started on 4000');
+  });
+});
+
+afterAll(() => {
+  app.close(() => {
+    console.log('server closed');
+  });
+});
+
+test('testing login endpoint, expect 200 status code', async () => {
+  const response = await axios.post('http://localhost:4000/api/login', {
     username: 'guest',
     password: 'guest',
   });
